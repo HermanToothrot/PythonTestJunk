e57c70c8aa06f1d017d619198f0722255d891ef0
HermanToothrot <45338989+HermanToothrot@users.noreply.github.com>
2023-08-13 21:03:14 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+print("hello world")
+universe_age = 14_000_000_000
+
+# When you print a number that was defined using underscores, 
+# Python prints only the digits:
+
+print(universe_age)
+import this # brings up some stuff
